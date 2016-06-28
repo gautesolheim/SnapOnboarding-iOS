f5a49cfda0f917dcc0c03a43dd96899a7ab8d9a1
Gaute Solheim <gautesolheim@icloud.com>
2016-06-28 14:13:38 +0200

    Setup Intro sparklingView

--- SnapOnboarding-iOSTests/SnapOnboarding_iOSTests.swift
+++ SnapOnboarding-iOSTests/SnapOnboarding_iOSTests.swift
@@ -29,8 +29,8 @@ class SnapOnboarding_iOSTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
-            // Put the code you want to measure the time of here.
         }
+        
     }
     
 }
