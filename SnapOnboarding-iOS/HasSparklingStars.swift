6d6086ec9e1c80294324564e4bf212e38b287770
Gaute Solheim <gautesolheim@icloud.com>
2016-07-12 23:42:16 +0200

    Randomize star sparkling speed

--- SnapOnboarding-iOS/HasSparklingStars.swift
+++ SnapOnboarding-iOS/HasSparklingStars.swift
@@ -13,8 +13,11 @@ extension HasSparklingStars {
             return
         }
         
+        let variation = duration < 1 ? 0 : 0.4
+        
         for star in sparklingStars {
-            let delay: NSTimeInterval = Double(arc4random_uniform(UInt32(duration) * 100)) * 0.01
+            let delay: NSTimeInterval = Double(arc4random_uniform(UInt32(duration * 100))) / 100 // 0 ... duration
+            let duration: NSTimeInterval = (duration - variation / 2) + Double(arc4random_uniform(UInt32(variation * 100))) / 100 // (duration - variation / 2) ... (duration + variation / 2)
             
             UIView.animateWithDuration(duration, delay: delay, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
                 star.transform = CGAffineTransformMakeScale(1.5, 1.5)
