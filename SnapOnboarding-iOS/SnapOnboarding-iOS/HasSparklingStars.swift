935a37c165eebb226fe0e2464b73d44a67505446
Gaute Solheim <gautesolheim@icloud.com>
2016-07-20 16:07:45 +0200

    Shorten almost-duplicate sparklingStars animation code

--- SnapOnboarding-iOS/SnapOnboarding-iOS/HasSparklingStars.swift
+++ SnapOnboarding-iOS/SnapOnboarding-iOS/HasSparklingStars.swift
@@ -19,26 +19,18 @@ extension HasSparklingStars {
             let delay: NSTimeInterval = Double(arc4random_uniform(UInt32(duration * 100))) / 100 // 0 ... duration
             let duration: NSTimeInterval = (duration - variation / 2) + Double(arc4random_uniform(UInt32(variation * 100))) / 100 // (duration - variation / 2) ... (duration + variation / 2)
             
-            UIView.animateWithDuration(duration, delay: delay, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
-                star.transform = CGAffineTransformMakeScale(1.5, 1.5)
-                }, completion: { _ in
-                    UIView.animateWithDuration(duration, delay: 0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
-                        star.transform = CGAffineTransformMakeScale(0.7, 0.7)
-                        }, completion: { _ in
-                            self.animateSparklingStarWithDuration(duration, star: star)
-                    })
-            })
+            animateSparklingStarWithDuration(duration, delay: delay, star: star)
         }
     }
     
-    private func animateSparklingStarWithDuration(duration: NSTimeInterval, star: UIImageView) {
-        UIView.animateWithDuration(duration, delay: 0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
+    private func animateSparklingStarWithDuration(duration: NSTimeInterval, delay: NSTimeInterval, star: UIImageView) {
+        UIView.animateWithDuration(duration, delay: delay, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
             star.transform = CGAffineTransformMakeScale(1.5, 1.5)
             }, completion: { _ in
                 UIView.animateWithDuration(duration, delay: 0, options: [UIViewAnimationOptions.CurveEaseInOut], animations: {
                     star.transform = CGAffineTransformMakeScale(0.7, 0.7)
                     }, completion: { _ in
-                        self.animateSparklingStarWithDuration(duration, star: star)
+                        self.animateSparklingStarWithDuration(duration, delay: 0, star: star)
                 })
         })
     }
